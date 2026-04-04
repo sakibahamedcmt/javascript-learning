31b6dc573b08fc251c813aa9cd1f11cc1762f45b
Sakib Ahamed <sakibahamedcmt@gmail.com>
2026-04-04 11:48:42 +0600

    task 1

--- day-01/task.js
+++ day-01/task.js
@@ -1,5 +1,20 @@
 // 3টি variable declare করো:
 
-var = "Sakib";
-let = 30;
+var name = "Sakib";
+let age = 30;
 const isDeveloper = true;
+
+
+// একটা object বানাও:
+
+
+  let person = {
+  name: "Sakib Ahamed",
+  age: 30,
+  skills: ["js", "html", "css"]
+}
+
+
+// একটা array বানাও তোমার 5 favorite foods নিয়ে
+
+let favoriteFoods = ["biriyani", "pizza", "burger", "noodles", "chocolate"];
