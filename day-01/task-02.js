f0f44639ba247dc59d2f005d401a5a26908d7e18
Sakib Ahamed <sakibahamedcmt@gmail.com>
2026-04-04 11:59:09 +0600

    tast 2 done

---
+++ day-01/task-02.js
@@ -0,0 +1,21 @@
+// Task 2 (Challenge)
+// একটা variable declare করো let myVar = 10
+// তারপর reassignment করো myVar = 20
+// চেষ্টা করো একইভাবে const দিয়ে reassignment করতে → কি হয় দেখো
+
+
+//একটা variable declare করো let myVar = 10
+
+let myVar = 10;
+console.log(myVar);
+
+
+// তারপর reassignment করো myVar = 20
+
+myVar = 20;
+console.log(myVar);
+
+
+// চেষ্টা করো একইভাবে const দিয়ে reassignment করতে → কি হয় দেখো 
+const myVar = 30;
+console.log(myVar)
