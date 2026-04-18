db1033dccabf861b672fd13263a5cbc80c4d973c
Sakib Ahamed <sakibahamedcmt@gmail.com>
2026-04-18 15:48:58 +0600

    day 02 complete

---
+++ day-02/task-01.js
@@ -0,0 +1,47 @@
+let num1 = 50;
+let num2 = 100;
+
+let sum;
+sum = num1+num2;
+console.log(sum);
+
+
+let minus;
+minus = num2-num1;
+console.log(minus);
+
+let multiple;
+multiple = num1*num2;
+console.log(multiple);
+
+let division;
+division = num1/num2;
+console.log(division);
+
+let moduals;
+moduals = num1%num2;
+console.log(moduals);
+
+let equal;
+equal = num1==num2;
+console.log(equal);
+
+let strictEquality;
+strictEquality = num1===num2;
+console.log(strictEquality);
+
+let notEquality;
+notEquality = num1!=num2;
+console.log(notEquality);
+
+let notStrictEquality;
+notStrictEquality = num1!==num2;
+console.log(notStrictEquality);
+
+let greaterThan;
+greaterThan = num1>num2;
+console.log(greaterThan);
+
+let lessThan;
+lessThan = num1<num2;
+console.log(lessThan);
---
+++ day-02/task-02.js
@@ -0,0 +1,21 @@
+let marks = 70;
+
+if(marks>=80 && marks <=100){
+    console.log("A+");
+} else if(marks>=70 && marks <=79){
+    console.log("A");
+}else if(marks>=60 && marks <=69){
+    console.log("B");
+}else if(marks>=50 && marks <=59){
+    console.log("C");
+} else if(marks>=40 && marks <=49){
+    console.log("D");
+} else {
+    console.log("Fail");
+}
+
+
+//Ternary operator দিয়ে test করো boolean variable
+
+let isPassed = marks >= 50 ? true : false;
+console.log("Passed:", isPassed);
